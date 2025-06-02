bd6e714b35fd5cd968dd6ce38f2012bd057ed574
Tushant Kaura <kauratushant@gmail.com>
2025-06-02 11:45:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let cityInput = document.getElementById("city_input"),
 searchBtn = document.getElementById("searchBtn")
 locationBtn = document.getElementById("locationBtn"),
-api_key = 'b009ad251b336037af8d972cc24dbb8c',
+api_key = 'API KEY',
 currentWeatherCard = document.querySelectorAll(".weather-left .card")[0],
 fiveDaysForecastCard = document.querySelector(".day-forecast"),
 aqiCard = document.querySelectorAll('.highlights .card')[0],
